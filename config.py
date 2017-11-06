95dd7411b977c9505cdf235282d1e3544075ed03
Brad Covey <bradcovey@gmail.com>
2017-11-05 20:31:22 -0400

    Added all of emodnet's wms and wfs services to the config

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
 MAPAPI_DATABASE_CONNECTOR = ''
+
+wms = [
+    'http://geoserver.emodnet-physics.eu/geoserver/emodnet/wms',
+    'http://drive.emodnet-geology.eu/geoserver/EMODnetGeology/wms',
+    'http://geoservice.maris2.nl/wms/seadatanet/emodnet_chemistry',
+    'http://77.246.172.208/geoserver/emodnet/wms',
+    'http://ec.oceanbrowser.net/emodnet/Python/web/wms',
+]
+
+wfs = [
+    'http://drive.emodnet-geology.eu/geoserver/EMODnetGeology/ows',
+    'http://geoserver.emodnet-physics.eu/geoserver/emodnet/wfs',
+    'http://geo.vliz.be/geoserver/Dataportal/ows?service=wfs',
+    'http://77.246.172.208/geoserver/emodnet/wfs',
+]
